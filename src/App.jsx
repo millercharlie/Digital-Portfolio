d6c191daa4974bde5ae826ecfa942254d20d9d72
Charlie Miller <charliemiller1016@gmail.com>
2024-08-02 16:24:06 -1000

    Change File Paths and Create new Design Portfolio Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Projects from "./pages/Projects.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
 import ProjectShowcase from "./pages/ProjectShowcase.jsx";
+import DesignPortfolio from "./pages/DesignPortfolio.jsx";
 
 /**
  * The App Component of this portfolio. This acts as the entry point for the user.
@@ -22,9 +23,9 @@ function App() {
         <Route path="projects" element={<Projects />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="project-one" element={<ProjectShowcase project={1} />} />
-        <Route path="project-two" element={<ProjectShowcase project={2} />} />
-        <Route path="project-three" element={<ProjectShowcase project={3} />} />
+        <Route path="projects/project-one" element={<ProjectShowcase project={1} />} />
+        <Route path="projects/project-two" element={<ProjectShowcase project={2} />} />
+        <Route path="projects/design-portfolio" element={<DesignPortfolio />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/DesignPortfolio.jsx
@@ -0,0 +1,7 @@
+export default function DesignPortfolio() {
+  return (
+    <>
+     <p>Design Portfolio under construction</p>
+    </>
+  )
+}
